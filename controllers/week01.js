b6cf409ed0c533f9f57702fce7d353799f8b7a0f
Leojr2021 <leo.meza.mancilla@gmail.com>
2024-01-10 22:49:03 -0500

    create the controller

---
+++ controllers/week01.js
@@ -0,0 +1,13 @@
+const RachelRoute = (req,res) =>{
+    res.send('Rachel Meza');
+};
+
+
+const VincentRoute = (req,res) =>{
+    res.send('Vincent Meza');
+};
+
+module.exports = {
+    RachelRoute,
+    VincentRoute,
+}
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const { VincentRoute, RachelRoute } = require('./controllers/week01');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Mario");
-});
+app.get('/', VincentRoute);
+app.get('/rachel', RachelRoute);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
