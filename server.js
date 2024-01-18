fbcc4c6208d05ffda2f2a1be0fbc61eeb2f8c1a2
Leojr2021 <leo.meza.mancilla@gmail.com>
2024-01-17 21:20:27 -0500

    feat/ add get,delete,put routes

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database')
 const app = express();
 
+// const mongoose = require('mongoose');
+
+
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 
 // app.listen(process.env.PORT || 3000, () => {
